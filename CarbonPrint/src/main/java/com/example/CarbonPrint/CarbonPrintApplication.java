d5b9fe9bd2d7ea242d82004dd9204f0be7f2f59d
ArthurFolly <arthurfollynf@gmail.com>
2026-04-23 21:32:02 -0300

    Inicio do projeto telas iniciais

---
+++ CarbonPrint/src/main/java/com/example/CarbonPrint/CarbonPrintApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarbonPrint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonPrintApplication.class, args);
+	}
+
+}
---
+++ CarbonPrint/src/test/java/com/example/CarbonPrint/CarbonPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarbonPrint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
